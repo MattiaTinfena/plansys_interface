0b5c40956bffb31dbffed00cc4ed3542fa3d3126
MattiaTinfena <mattia.tinfena@gmail.com>
2026-01-19 16:33:43 +0100

    Added alignment

--- src/go_to_point_action.cpp
+++ src/go_to_point_action.cpp
@@ -1,20 +1,25 @@
+#include "cv_bridge/cv_bridge.hpp"
 #include "geometry_msgs/msg/pose.hpp"
+#include "image_transport/image_transport.hpp"
 #include "nav2_msgs/action/navigate_to_pose.hpp"
 #include "nav_msgs/msg/odometry.hpp"
 #include "plansys_interface/action/go_to_point.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
-#include <memory>
+#include <opencv2/aruco.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using GoToPoint = plansys_interface::action::GoToPoint;
+using GoalHandleGoToPoint = rclcpp_action::ServerGoalHandle<GoToPoint>;
 
 using namespace std::chrono_literals;
+bool align;
+
+rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr velocity_publisher;
+
 class GoToPointNode : public rclcpp::Node {
   public:
-	using GoToPoint = plansys_interface::action::GoToPoint;
-	using GoalHandleGoToPoint = rclcpp_action::ServerGoalHandle<GoToPoint>;
-
 	GoToPointNode() : rclcpp::Node("GoToPointNode") {
-
-		// nav2_node_ = rclcpp::Node::make_shared("move_action_nav2_client");
 
 		nav2_client_ =
 			rclcpp_action::create_client<nav2_msgs::action::NavigateToPose>(
@@ -53,9 +58,9 @@ class GoToPointNode : public rclcpp::Node {
 					result_msg->success = false;
 					this->goal_handle->succeed(result_msg);
 				} else {
-					RCLCPP_INFO(get_logger(), "Navigation succeded:");
+					RCLCPP_INFO(get_logger(), "Aligning");
 					result_msg->success = true;
-					this->goal_handle->succeed(result_msg);
+					align = true;
 				}
 			};
 		rclcpp_action::Client<nav2_msgs::action::NavigateToPose>::
@@ -74,35 +79,105 @@ class GoToPointNode : public rclcpp::Node {
 
 		this->action_server_ = rclcpp_action::create_server<GoToPoint>(
 			this, "go_to_point", handle_goal, handle_cancel, handle_accepted);
-
-		// rclcpp::spin_some(nav2_node_);
 	}
+	std::shared_ptr<GoalHandleGoToPoint> goal_handle;
 
   private:
-	// void execute(const std::shared_ptr<GoalHandleGoToPoint> goal_handle) {
-	// 	RCLCPP_INFO(this->get_logger(), "Executing GoToPoint action");
-
-	// 	auto result = std::make_shared<GoToPoint::Result>();
-	// 	result->success = true;
-
-	// 	goal_handle->succeed(result);
-	// }
-
 	rclcpp_action::Server<GoToPoint>::SharedPtr action_server_;
-	// rclcpp::Node::SharedPtr nav2_node_;
 	rclcpp_action::Client<nav2_msgs::action::NavigateToPose>::SharedPtr
 		nav2_client_;
-	std::shared_ptr<GoalHandleGoToPoint> goal_handle;
 };
+
+std::shared_ptr<GoToPointNode> go_to_point_node;
+
+void image_callback(const sensor_msgs::msg::Image::ConstSharedPtr &msg) {
+	if (!align) {
+		return;
+	}
+
+	try {
+		cv::Mat img = cv_bridge::toCvShare(msg, "bgr8")->image;
+		std::vector<int> markerIds;
+		std::vector<std::vector<cv::Point2f>> markerCorners, rejectedCandidates;
+		cv::Ptr<cv::aruco::DetectorParameters> parameters =
+			cv::aruco::DetectorParameters::create();
+		cv::Ptr<cv::aruco::Dictionary> dictionary =
+			cv::aruco::getPredefinedDictionary(cv::aruco::DICT_ARUCO_ORIGINAL);
+		cv::aruco::detectMarkers(img, dictionary, markerCorners, markerIds,
+								 parameters, rejectedCandidates);
+
+		geometry_msgs::msg::Point message;
+
+		int x_center = 0, y_center = 0;
+
+		if (markerIds.size() == 1) {
+			message.z = markerIds[0];
+
+			for (int i = 0; i < markerCorners[0].size(); i++) {
+				x_center += markerCorners[0][i].x;
+				y_center += markerCorners[0][i].y;
+			}
+			x_center /= 4;
+			y_center /= 4;
+		}
+
+		const int x_error = 640 / 2 - x_center;
+
+		geometry_msgs::msg::Twist twist{};
+
+		if (std::abs(x_error) < 20) {
+			align = false;
+			std::cout << "CORRECTLY ALIGNED" << std::endl;
+			velocity_publisher->publish(twist);
+			auto result_msg = std::make_shared<GoToPoint::Result>();
+			result_msg->success = true;
+			go_to_point_node->goal_handle->succeed(result_msg);
+			return;
+		}
+
+		if (x_error < 0) {
+			twist.angular.z = -0.2;
+		} else {
+			twist.angular.z = 0.2;
+		}
+		velocity_publisher->publish(twist);
+
+	} catch (cv_bridge::Exception &e) {
+		RCLCPP_ERROR(rclcpp::get_logger("subscriber"),
+					 "cv_bridge exception: %s", e.what());
+		return;
+	}
+}
 
 int main(int argc, char **argv) {
 	rclcpp::init(argc, argv);
 
-	auto node = std::make_shared<GoToPointNode>();
+	go_to_point_node = std::make_shared<GoToPointNode>();
+
+	rclcpp::NodeOptions options;
+	auto image_listener_node =
+		rclcpp::Node::make_shared("image_listener", options);
+	image_listener_node->declare_parameter<std::string>("image_transport",
+														"compressed");
+
+	image_transport::ImageTransport it(image_listener_node);
+	image_transport::TransportHints hints(image_listener_node.get());
+
+	image_transport::Subscriber sub =
+		it.subscribe("camera/image", 1, image_callback, &hints);
+	velocity_publisher =
+		image_listener_node->create_publisher<geometry_msgs::msg::Twist>(
+			"/cmd_vel", 10);
 
 	RCLCPP_INFO(rclcpp::get_logger("rclcpp"),
 				"Ready to reach the point and align.");
 
-	rclcpp::spin(node);
+	rclcpp::executors::SingleThreadedExecutor executor;
+
+	executor.add_node(go_to_point_node->get_node_base_interface());
+	executor.add_node(image_listener_node->get_node_base_interface());
+
+	executor.spin();
+
 	rclcpp::shutdown();
 }
